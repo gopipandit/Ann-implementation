20710dd7202978d3e55ed213ae399448f455e7ee
gopipandit <gopi1035@gmail.com>
2023-02-17 01:07:58 +0530

    files Updated

--- src/utils/data_mgmt.py
+++ src/utils/data_mgmt.py
@@ -1,13 +1,12 @@
 import tensorflow as tf
 
 
-def get_data(validation_data_size):
+def get_data(validation_datasize):
     mnist = tf.keras.datasets.mnist
-    (X_train_full, y_train_full), (X_val,
-                                   y_val), (x_test, y_test) = mnist.load_data()
+    (X_train_full, y_train_full), (x_test, y_test) = mnist.load_data()
 
-    X_valid, X_train = X_train_full[:validation_data_size] / \
-        255., y_train[validation_data_size:] / 255.
-    y_valid, y_train = y_train_full[:validation_data_size], y_train_full[validation_data_size:]
+    X_valid, X_train = X_train_full[:validation_datasize] / \
+        255., y_train[validation_datasize:] / 255.
+    y_valid, y_train = y_train_full[:validation_datasize], y_train_full[validation_datasize:]
     x_test = x_test/255.
     return (X_train, y_train), (X_valid, y_valid), (x_test, y_test)
